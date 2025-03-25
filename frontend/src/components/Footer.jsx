43fc16525bcf3b71dd1fbf2c2b7a5f721fa5509b
Nekal Singh Salaria <nekalsingh987@gmail.com>
2025-03-25 15:10:12 +0530

    .

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import "../css/footer.css";
 const Footer = () => {
   return (
     <footer>
-      <p>&copy; 2025 Manoharpura Mokshdham Vikas Samiti</p>
+      <p>&copy; 2025 Manoharpura Mokshdham Vikas</p>
       <p>Developed by Nekal Singh - <a href="https://linkedin.com/in/nekalsingh" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
     </footer>
   );

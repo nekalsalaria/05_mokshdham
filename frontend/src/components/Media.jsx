1ccc7785e1cb7ec492e38f7dd7be40e92593dd8f
Nekal Singh Salaria <nekalsingh987@gmail.com>
2025-03-26 11:55:16 +0530

    make some changes in the meida coverage

--- frontend/src/components/Media.jsx
+++ frontend/src/components/Media.jsx
@@ -3,17 +3,21 @@ import "../css/media.css";
 import media1 from "../images/media1.jpg";
 import media2 from "../images/media2.jpg";
 import media3 from "../images/media3.jpg";
-import media4 from "../images/media4.jpg";
 
 const Media = () => {
   return (
     <div className="media">
       <h2>Media Coverage</h2>
-      <p>Check out what the media says about us!</p>
       <div className="media-images">
-        <img src={media1} alt="Media 1" />
-        <img src={media2} alt="Media 2" />
-        <img src={media3} alt="Media 3" />
+        <div className="media-item">
+          <img src={media1} alt="Media 1" />
+        </div>
+        <div className="media-item">
+          <img src={media2} alt="Media 2" />
+        </div>
+        <div className="media-item">
+          <img src={media3} alt="Media 3" />
+        </div>
       </div>
     </div>
   );
